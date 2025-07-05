4eb35fe227495c201ab65e9dc05801c61232cd05
Noovad <novadwynt28@gmail.com>
2025-07-06 02:20:23 +0700

    Remove dist from repo and add to .gitignore"

--- dist/configs/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_EXPIRES_IN = exports.JWT_SECRET = exports.DATABASE_URL = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-_a = process.env, exports.DATABASE_URL = _a.DATABASE_URL, exports.JWT_SECRET = _a.JWT_SECRET, exports.JWT_EXPIRES_IN = _a.JWT_EXPIRES_IN;
